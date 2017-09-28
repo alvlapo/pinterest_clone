58bb6c167d7cebbd85306b4f00412d6b49c12d2f
alvlapo <alvlapo@gmail.com>
2017-09-28 11:12:53 +0300

    Rspec init setup

--- backend/src/config/application.rb
+++ backend/src/config/application.rb
@@ -20,14 +20,17 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_spec false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config.autoload_paths += %W(\#{config.root}/lib)
   end
 end
